146effb09ff210f4f081fed51a9cd49cf0318627
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-11-30 15:08:52 +0900

    Revert "feat: refresh token 구현"

--- backend/demo/src/main/java/com/snacks/demo/jwt/JwtAuthorizationFilter.java
+++ backend/demo/src/main/java/com/snacks/demo/jwt/JwtAuthorizationFilter.java
@@ -1,16 +1,11 @@
 package com.snacks.demo.jwt;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.snacks.demo.config.EnvConfiguration;
 import com.snacks.demo.entity.User;
 import com.snacks.demo.jwt.auth.CustomUserDetails;
 import com.snacks.demo.repository.AuthRepository;
-import com.snacks.demo.response.ResponseService;
 import java.io.IOException;
 import java.util.Optional;
 import javax.servlet.FilterChain;
@@ -18,7 +13,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -42,68 +36,33 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain chain) throws IOException, ServletException {
 
-    String servletPath = request.getServletPath();
     String header = request.getHeader(env.getProperty("header_string"));
-
-    //로그인, 리프레시 요청이라면 토큰 검사 안함
-    if (servletPath.equals("/auth/login") || servletPath.equals("/auth/refresh")
-        || servletPath.equals("/auth")) {
+    if (header == null || !header.startsWith(env.getProperty("token_prefix"))) {
       chain.doFilter(request, response);
+      return;
     }
+    String token = request.getHeader(env.getProperty("header_string"))
+        .replace(env.getProperty("token_prefix"), "");
 
-    //토큰 값이 없거나 정상적이지 않으면?
-    else if (header == null || !header.startsWith(env.getProperty("token_prefix"))) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-      response.setCharacterEncoding("UTF-8");
-      ResponseService responseService = new ResponseService();
-      ObjectMapper mapper = new ObjectMapper();
-      try {
-        mapper.writeValue(response.getWriter(),
-            responseService.errorResponse("JWT 토큰이 존재하지 않습니다."));
-      } catch (IOException ex) {
-        throw new RuntimeException(ex);
-      }
-    } else {
-      try {
-        String token = request.getHeader(env.getProperty("header_string"))
-            .replace(env.getProperty("token_prefix"), "");
+    String email = JWT.require(Algorithm.HMAC512(env.getProperty("secret"))).build().verify(token)
+        .getClaim("email").asString();
 
-        //access token 검증
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC512(env.getProperty("secret"))).build();
-        DecodedJWT decodedJWT = jwtVerifier.verify(token);
-
-        //authetication 객체 생성
-        String email = decodedJWT.getSubject();
-        Optional<User> user = authRepository.findByEmail(email);
-        CustomUserDetails customUserDetails = new CustomUserDetails(user.get());
-        Authentication authentication =
-            new UsernamePasswordAuthenticationToken(
-                customUserDetails,
-                null,
-                customUserDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+    if (email != null) {
+      Optional<User> user = authRepository.findByEmail(email);
+      if (user.isEmpty()) {
         chain.doFilter(request, response);
-
-
-      } catch (TokenExpiredException expiredException) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding("UTF-8");
-        ResponseService responseService = new ResponseService();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getWriter(),
-            responseService.errorResponse("Access Token이 만료되었습니다."));
-
-      } catch (Exception e) {
-        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setCharacterEncoding("UTF-8");
-        ResponseService responseService = new ResponseService();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(response.getWriter(), responseService.errorResponse("잘못된 JWT 토큰입니다."));
-
       }
+
+      CustomUserDetails customUserDetails = new CustomUserDetails(user.get());
+      Authentication authentication =
+          new UsernamePasswordAuthenticationToken(
+              customUserDetails,
+              null,
+              customUserDetails.getAuthorities());
+
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+
     }
+    chain.doFilter(request, response);
   }
 }
--- backend/demo/src/test/java/com/snacks/demo/Auth/LoginTest.java
+++ backend/demo/src/test/java/com/snacks/demo/Auth/LoginTest.java
@@ -62,7 +62,94 @@ public class LoginTest {
     factory.close();
   }
 
+  // 테스트 케이스
+  static Stream<Arguments> loginProvider() throws Throwable {
+    return Stream.of(
+        Arguments.arguments(ConstantTest.VALID_EMAIL, ConstantTest.VALID_PASSWORD, "정상 케이스", true),
+        Arguments.arguments("", "1234", "empty 이메일", false),
+        Arguments.arguments(null, "1234", "null 이메일", false),
+        Arguments.arguments("test@test.com", "", "empty 비밀번호", false),
+        Arguments.arguments("test@test.com", null, "null 비밀번호", false),
+        Arguments.arguments("", "", "empty 이메일, empty 비밀번호", false),
+        Arguments.arguments(null, null, "null 이메일, null 비밀번호", false),
+        Arguments.arguments("test", "1234", "잘못된 이메일 형식", false),
+        Arguments.arguments("test", "", "잘못된 이메일 형식, null 비밀번호", false),
+        Arguments.arguments("test", null, "잘못된 이메일 형식, empty 비밀번호", false)
+    );
+  }
+
+  static Stream<Arguments> loginServiceProvider() throws Throwable {
+    return Stream.of(
+        Arguments.arguments(ConstantTest.VALID_EMAIL, ConstantTest.VALID_PASSWORD, "로그인 성공", true),
+        Arguments.arguments(ConstantTest.VALID_EMAIL, "0000", "비밀번호 다름", false),
+        Arguments.arguments("fake@test.com", "1234", "없는 이메일", false)
+    );
+  }
+
+  @ParameterizedTest(name = "{index} - {3} - {2}")
+  @DisplayName("login validation 테스트")
+  @MethodSource("loginProvider")
+  void signUpValidation(String email, String password, String message, boolean expected) {
+    //given
+    UserDto userDto = new UserDto(email, password);
+
+    //when
+    Set<ConstraintViolation<UserDto>> violations = validator.validate(userDto);
+
+    //then
+    for (ConstraintViolation<UserDto> violation : violations) {
+      System.out.println(violation.getMessage());
+    }
+    if (expected == true) {
+      assertThat(violations.size()).isEqualTo(0);
+    } else {
+      assertThat(violations.size()).isNotEqualTo(0);
+    }
+  }
+
+  // service 테스트
+  @ParameterizedTest(name = "{index} - {3} - {2}")
+  @DisplayName("login Service 테스트")
+  @MethodSource("loginServiceProvider")
+  void loginService(String email, String password, String message, boolean expected) {
+    //given
+    UserDto userDto = new UserDto(email, password);
+
+    //when
+    ResponseEntity responseEntity = authService.login(userDto);
+
+    //then
+    if (expected == true) {
+      assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    } else {
+      assertThat(responseEntity.getStatusCode()).isNotEqualTo(HttpStatus.OK);
+    }
+  }
+
   // controller 테스트
+  @DisplayName("POST /auth 테스트")
+  @Test
+  void loginController() throws Exception {
+    //given
+    authService.signUp(new UserDto(ConstantTest.VALID_EMAIL, ConstantTest.VALID_PASSWORD));
+    String user = objectMapper.writeValueAsString(
+        new UserDto(ConstantTest.VALID_EMAIL, ConstantTest.VALID_PASSWORD));
+
+    //when
+
+    //then
+    RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/auth/login")
+        .contentType(org.springframework.http.MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .content(user);
+
+    mockMvc.perform(requestBuilder)
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andDo(MockMvcResultHandlers.print())
+        .andReturn()
+        .getResponse();
+  }
+
   @DisplayName("토큰 발급 테스트")
   @Test
   void create_token_test() throws Exception {
