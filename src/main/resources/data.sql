8656fc7589c5d878e0a3d6e67d380bca4695ddaa
jueun <57864944+dlwndms0812@users.noreply.github.com>
2022-12-22 15:22:59 +0900

    feat/실행시-sql-구문-자동실행 (#37)
    - 실행시에 위젯 데이터 자동으로 삽입

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO widget (name) VALUES ('memo');
+INSERT INTO widget (name) VALUES ('ascii');
+INSERT INTO widget (name) VALUES ('weather');
+INSERT INTO widget (name) VALUES ('todo');
+INSERT INTO widget (name) VALUES ('timer');
