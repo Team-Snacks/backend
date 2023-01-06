0beec9ed71442a29f92c86cd2a16644e3dc75d4b
jueun <57864944+dlwndms0812@users.noreply.github.com>
2023-01-06 14:41:15 +0900

    remove: data.sql 삭제 (#49)
    - rds를 연결하며 더이상 서버를 실행하면서 테이블을 내리고 다시 올릴 필요가 없기에 해당 파일도 필요가 없음
    - rds에 해당 data가 영구적으로 들어가 있기 때문에 삭제

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO widget (name, description, image) VALUES ('memo', '간단한 메모를 할 수 있습니다', 'https://snacks42.s3.ap-northeast-2.amazonaws.com/memo.png');
-INSERT INTO widget (name, description, image) VALUES ('timer', '타이머를 설정할 수 있습니다', 'https://snacks42.s3.ap-northeast-2.amazonaws.com/timer.png');
-INSERT INTO widget (name, description, image) VALUES ('ascii', '아스키 코드 표를 참조합니다', 'https://snacks42.s3.ap-northeast-2.amazonaws.com/ascii.png');
